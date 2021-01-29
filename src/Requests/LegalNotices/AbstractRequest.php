693b2306f8d774dbdfc04922043109adfc32678b
Pierangelo <pierangelo90@gmail.com>
2021-01-29 17:40:47 +0100

    Fixed declaration incompatibility

--- src/Requests/LegalNotices/AbstractRequest.php
+++ src/Requests/LegalNotices/AbstractRequest.php
@@ -4,7 +4,7 @@ namespace SoluzioneSoftware\Iubenda\Requests\LegalNotices;
 
 abstract class AbstractRequest extends \SoluzioneSoftware\Iubenda\Requests\AbstractRequest
 {
-    protected function getUrl(string $path = '/')
+    protected function getUrl(string $path = '/'): string
     {
         return parent::getUrl('/legal_notices' . $path);
     }
