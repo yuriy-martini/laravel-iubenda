e3025a8e310d630ff6271b51941cfe294af2aad9
Yuriy Martini <hi@ymartini.me>
2022-06-07 11:33:54 +0200

    implemented Legal Notice StoreRequest

--- src/Requests/LegalNotices/StoreRequest.php
+++ src/Requests/LegalNotices/StoreRequest.php
@@ -2,17 +2,57 @@
 
 namespace SoluzioneSoftware\Iubenda\Requests\LegalNotices;
 
+use Carbon\Carbon;
 use Exception;
-use SoluzioneSoftware\Iubenda\Responses\SubjectsStoreResponse;
+use GuzzleHttp\Exception\GuzzleException;
+use SoluzioneSoftware\Iubenda\Responses\LegalNoticeStoreResponse;
 
 class StoreRequest extends AbstractRequest
 {
+
     /**
-     * @return SubjectsStoreResponse
+     * ISO 8601 timestamp of the Legal Notice<br>
+     * auto-filled if not provided
+     *
+     * @param  Carbon  $value
+     * @return StoreRequest
+     */
+    public function timestamp(Carbon $value): self
+    {
+        return $this->withBody('timestamp', $value->toIso8601ZuluString());
+    }
+
+    /**
+     * Saves the passed identifier on the Legal Notice.
+     * Default null
+     *
+     * @param string $value
+     * @return StoreRequest
+     */
+    public function identifier(string $value): self
+    {
+        return $this->withBody('identifier', $value);
+    }
+
+    /**
+     * Saves the passed content(s) on the Legal Notice.
+     * Default null
+     *
+     * @param string|array $value
+     * @return StoreRequest
+     */
+    public function content($value): self
+    {
+        return $this->withBody('content', $value);
+    }
+
+    /**
+     * @return LegalNoticeStoreResponse
+     * @throws GuzzleException
      * @throws Exception
      */
-    public function execute(): SubjectsStoreResponse
+    public function execute(): LegalNoticeStoreResponse
     {
-        throw new Exception('Not implemented');
+        return LegalNoticeStoreResponse::create(json_decode($this->executePost()->getBody(), true));
     }
 }
